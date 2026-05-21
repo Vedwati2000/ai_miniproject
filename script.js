d406adb8285699371c0be5f5f5bd75e05d33e885
Vedwati2000 <vedwatipradhan23@navgurukul.org>
2026-05-21 12:04:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const askBtn = document.getElementById("askBtn");
+
+askBtn.addEventListener("click", async () => {
+
+    const question =
+        document.getElementById("question").value;
+
+    const result =
+        document.getElementById("result");
+
+    if(question === ""){
+        alert("Please enter question");
+        return;
+    }
+
+    result.innerText = "Loading...";
+
+    try {
+
+        const response = await fetch("/ask-gemini", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                question: question
+            })
+
+        });
+
+        const data = await response.json();
+
+        result.innerText = data.reply;
+
+    }
+
+    catch(error){
+
+        console.log(error);
+
+        result.innerText =
+        "Something went wrong";
+
+    }
+
+});
